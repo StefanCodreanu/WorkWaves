51ded15c293ac5089374d8f44a089d3d90b38f6a
StefanCodreanu <stefancodreanu21@gmail.com>
2024-01-09 17:09:45 -0500

    new start

--- lib/features/authentication/domain/authentication_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:workwaves/core/entities/user.dart';
-
-abstract class AuthenticationRepository {
-  Future<Human?> getCurrentUser();
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-
-  isUserSignedIn() {}
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:workwaves/features/authentication/presentation/authentication_bloc.dart';
-import 'package:workwaves/features/authentication/presentation/authentication_page.dart';
-import 'package:workwaves/injection_container.dart';
+import 'package:workwaves/pages/login_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  init(); // Initialize dependency injection container
   runApp(const MyApp());
 }
 
@@ -22,10 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (context) => sl<AuthenticationBloc>(),
-        child: const AuthenticationPage(),
-      ),
+      home: const LoginPage(),
     );
   }
 }
